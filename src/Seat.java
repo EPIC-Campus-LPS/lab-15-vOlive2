2135da4a3b31088ae869930542f777d9d2b93f61
vOlive2.0 <OLiveA148@lpsk12.org>
2025-10-30 10:58:48 -0600

    a

--- src/Seat.java
+++ src/Seat.java
@@ -1,9 +1,12 @@
-
 public class Seat {
 	private boolean sold;
 	private String type;
 	private double price;
-	public Seat() {
-		
+	public Seat(boolean s, String t, double p) {
+		sold = s;
+		type = t;
+		price = p;
 	}
+	public boolean avalable() {return sold;}
+	public void buySeat() {sold = true;}
 }
--- src/Venue.java
+++ src/Venue.java
@@ -1,3 +1,32 @@
 public class Venue {
+	Seat[][] seats;
+	Venue(int row, int col, double price) {
+		seats = new Seat[row][col];
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < col; j++) {
+				seats[i][j] = new Seat(false, "R", price);
+			}
+		}
+	}
+	
+	public boolean buyTicket(int row, int col) {
+		if(seats[row][col].avalable()) seats[row][col].buySeat();
+	}
+	
+	private boolean isAvailable(int row, int col)
+	public void setPremium(int row, double price)
+	public void setPremium(int row, int colStart, int colEnd, double price)
+	public void setGA(int row, double price)
+	public boolean importTickets(String filename) throws IOException
+	public double totalRevenue()
+	public double totalRevenue(int col)
+	public int totalSold()
+	public int totalSold(int row)
+	public void printVenue()
+	public void printVenueType()
+	public void printVenuePrice()
+	public double maxPrice(int rowStart, int rowEnd, int colStart, int colEnd)
+	public boolean containsGA(int row)
+	public boolean allPremium(int col)
 
 }
