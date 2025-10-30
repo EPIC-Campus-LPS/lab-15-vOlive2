8a1fd56d48b37d82abdac309a15f29143c01e86f
vOlive2.0 <OLiveA148@lpsk12.org>
2025-10-30 11:20:53 -0600

    a

--- src/Seat.java
+++ src/Seat.java
@@ -8,5 +8,11 @@ public class Seat {
 		price = p;
 	}
 	public boolean avalable() {return sold;}
+	public int soldNum() {if(sold){return 1;} return 0;}
 	public void buySeat() {sold = true;}
+	public double getPrice() {return price;}
+	public String getType() {return type;}
+	public void pre(double p) {type="P"; price=p;}
+	public void gen(double p) {type="G"; price=p;}
+	@Override public String toString() {return " |"+type+"("+sold+")| ";}
 }
--- src/Venue.java
+++ src/Venue.java
@@ -9,24 +9,68 @@ public class Venue {
 		}
 	}
 	
-	public boolean buyTicket(int row, int col) {
-		if(seats[row][col].avalable()) seats[row][col].buySeat();
+	public boolean buyTicket(int row, int col) {if(seats[row][col].avalable()) {seats[row][col].buySeat();return true;}return false;}
+	private boolean isAvailable(int row, int col) {return seats[row][col].avalable();}	
+	public void setPremium(int row, double price) {for(int i = 0; i < seats.length; i++) {seats[row][i].pre(price);}}
+	public void setPremium(int row, int colStart, int colEnd, double price) {for(int i = colStart; i < colEnd; i++) {seats[row][i].pre(price);}}
+	public void setGA(int row, double price) {for(int i = 0; i < seats.length; i++) {seats[row][i].gen(price);}}
+	//public boolean importTickets(String filename) throws IOException {}
+	
+	public double totalRevenue() {
+		double rev = 0.0;
+		for(int i = 0; i < seats.length; i++) {
+			for(int j = 0; j < seats[i].length; j++) {
+				rev += seats[i][j].getPrice();
+			}
+		}
+		return rev;
+	}
+	public double totalRevenue(int col) {
+		double rev = 0.0;
+		for(int i = 0; i < seats.length; i++) {
+			rev += seats[i][col].getPrice();
+		}
+		return rev;
+		
+	}
+	public int totalSold() {
+		int sold = 0;
+		for(int i = 0; i < seats.length; i++) {
+			for(int j = 0; j < seats[i].length; j++) {
+				sold += seats[i][j].soldNum();
+			}
+		}
+		return sold;
+	}
+	public int totalSold(int row) {
+		int sold = 0;
+		for(int i = 0; i < seats.length; i++) {
+			sold += seats[row][i].soldNum();
+		}
+		return sold;
+	}
+	public void printVenue() {
+		for(Seat[] s : seats) {
+			for(Seat t : s) {
+				System.out.print(t.avalable());
+			}
+			System.out.print("\n");
+		}
+	}
+	public void printVenueType() {
+		for(Seat[] s : seats) {
+			for(Seat t : s) {
+				System.out.print(t.getType());
+			}
+			System.out.print("\n");
+		}
+	}
 	}
 	
-	private boolean isAvailable(int row, int col)
-	public void setPremium(int row, double price)
-	public void setPremium(int row, int colStart, int colEnd, double price)
-	public void setGA(int row, double price)
-	public boolean importTickets(String filename) throws IOException
-	public double totalRevenue()
-	public double totalRevenue(int col)
-	public int totalSold()
-	public int totalSold(int row)
-	public void printVenue()
-	public void printVenueType()
-	public void printVenuePrice()
-	public double maxPrice(int rowStart, int rowEnd, int colStart, int colEnd)
-	public boolean containsGA(int row)
-	public boolean allPremium(int col)
+	public void printVenuePrice() {}
+	
+	public double maxPrice(int rowStart, int rowEnd, int colStart, int colEnd) {}
+	public boolean containsGA(int row) {}
+	public boolean allPremium(int col) {}
 
 }
